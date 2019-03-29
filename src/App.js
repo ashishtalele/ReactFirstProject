1d0361d2301b1c0163bf88ea1b6544b12d686354
Ashish Talele <ashishytalele@gmail.com>
2019-03-30 00:59:25 +0530

    2 created MainRouter and other architecture

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Error from './components/Error';
+import React, { Component, Fragment } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MainRouter from './components/MainRouter';
+
 class App extends Component {
   render() {
-    // exact: use only exact path
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" component={Home} exact/>
-          <Route path="/About" component={About} />
-          <Route path="/Contact" component={Contact} />
-          <Route component={Error} exact/>
-        </Switch>
-      </BrowserRouter>
+      <Fragment>
+        <MainRouter />
+      </Fragment>
     );
   }
 }
